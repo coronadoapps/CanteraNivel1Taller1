3e55e185e250e8d035ef48b19c445db6318a321c
stiven c <iamalescoronado@gmail.com>
2022-02-26 13:21:41 -0500

    Remote repository

--- src/com/sofka/Main.java
+++ src/com/sofka/Main.java
@@ -16,7 +16,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         do {
-            limpiarPantalla();
+            limpiarPantalla(); //Metodo limpiar pantalla
             menu();
             opcion = capturarOpcion();
             limpiarPantalla();
